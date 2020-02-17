d4ee4bb9ecf9ba8716578394f7f9df043442de45
Denise Magner <deerenae@gmail.com>
2020-02-17 00:00:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   array = string.split(",")
   new_array = []
   array.each do |string|
-    new_array  
+    new_array  << string.reverse 
   end 
+  new_array.join 
 end   
